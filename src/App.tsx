6e4e3ad0ff3c589c99f38f7978da1643ad07c735
Jorge Chavez <jchavezpalma@bloomberg.net>
2024-05-13 00:11:13 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import LandingPage from "./components/LandingPage/LandingPage"
+import './App.css';
+
+export const App = () => {
+
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { PaletteMode } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import AppAppBar from './components/AppAppBar';
+import Hero from './components/Hero';
+import getLPTheme from './getLPTheme';
+
+export const LandingPage = () => {
+  const [mode, setMode] = React.useState<PaletteMode>('light');
+  const LPtheme = createTheme(getLPTheme(mode));
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  return (
+    <ThemeProvider theme={LPtheme}>
+      <CssBaseline />
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      <Hero />
+    </ThemeProvider>
+  );
+}
+
+
+export default LandingPage;
